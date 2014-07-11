2b01678c094e70cd31aca7a09ed83b9b694942cd
Nishitani <nishikei@6476bab452b4.ant.amazon.com>
2014-07-11 11:28:35 +0900

    enable to get a message

--- lib/reader.rb
+++ lib/reader.rb
@@ -2,18 +2,25 @@ require 'aws-sdk'
 
 access_key = ENV['ACCESS_KEY_ID']
 secret_access_key = ENV['SECRET_ACCESS_KEY']
+@queue_name = "sample20140710"
 
 AWS.config(
 	:access_key_id => access_key,
-	:secret_access_key => secret_access_key)
+	:secret_access_key => secret_access_key,
+	:region => "ap-northeast-1")
 
 def get
 	sqs = AWS::SQS.new
-	queues = sqs.queues
 
-	queues.each {|queue|
-		p queue.name
-	}
+	# queues = sqs.queues
+	# queues.each {|queue|
+	# 	puts queue.url
+	# }
+
+	queue = sqs.queues.named(@queue_name)
+	queue.poll do |msg|
+		puts msg.body
+	end
 
 end
 
